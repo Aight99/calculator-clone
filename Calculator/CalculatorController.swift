3cb567d58e3a89f3e9c307f4f17bf73c3b68080b
Alexander Ilchenko <2chenko.as@gmail.com>
2023-02-05 18:53:49 +1000

    Initial commit

---
+++ Calculator/CalculatorController.swift
@@ -0,0 +1,19 @@
+//
+//  CalculatorController.swift
+//  Calculator
+//
+//  Created by ios_developer on 05.02.2023.
+//
+
+import UIKit
+
+class CalculatorController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
